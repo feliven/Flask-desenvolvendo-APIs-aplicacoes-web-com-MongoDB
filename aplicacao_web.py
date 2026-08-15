605e123d55d627b3c65a2619c4c2f9ad9c505b0d
FVS-MBA <feliven@gmail.com>
2026-08-15 15:56:01 -0300

    separa HTML do arquivo Python

--- aplicacao_web.py
+++ aplicacao_web.py
@@ -3,21 +3,11 @@ from wsgiref.simple_server import make_server
 
 def app(environ, start_response):
     start_response("200 OK", [("Content-Type", "text/html; charset=utf-8")])
-    html = b"""
-    <!doctype html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>WSGI</title>
-    </head>
-    <body>
-        WSGI
-    </body>
-    </html>
-    """
 
-    return [html]
+    with open("index.html", "r", encoding="utf-8") as pagina:
+        html = pagina.read()
+
+    return [html.encode("utf-8")]
 
 
 make_server("", 5001, app).serve_forever()
