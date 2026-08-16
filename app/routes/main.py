13d3e975bafcf071dfd0f8721a1c2d6547a6cea7
FVS-MBA <feliven@gmail.com>
2026-08-16 08:31:35 -0300

    conecta ao banco de dados e implementa find()

--- app/routes/main.py
+++ app/routes/main.py
@@ -1,6 +1,8 @@
 from flask import Blueprint, jsonify, request
 from app.models.login_payload import LoginPayload
 from pydantic import ValidationError
+from app import db
+from bson import ObjectId
 
 main_bp = Blueprint("main_bp", __name__)
 
@@ -11,40 +13,48 @@ def index():
 
 
 # RF: O sistema deve permitir listagem de todos os produtos
-@main_bp.route("/products")
-def get_products():
-    return jsonify({"message": "Esta é a rota de listagem dos produtos"})
+@main_bp.route("/produtos")
+def get_produtos():
+    produtos_cursor = db.produtos.find({})
+
+    lista_produtos = []
+
+    for produtos in produtos_cursor:
+        produtos["_id"] = str(produtos["_id"])
+        lista_produtos.append(produtos)
+
+    return jsonify(lista_produtos)
 
 
 # RF: O sistema deve permitir a criacao de um novo produto
-@main_bp.route("/products", methods=["POST"])
-def create_product():
+@main_bp.route("/produtos", methods=["POST"])
+def create_produto():
     return jsonify({"message": "Esta é a rota de criação de produto"})
 
 
 # RF: O sistema deve permitir a visualizacao dos detalhes de um unico produto
-@main_bp.route("/product/<int:product_id>")
-def get_product_by_id(product_id):
+@main_bp.route("/produto/<int:id_produto>")
+def get_produto_by_id(id_produto):
     return jsonify(
         {
-            "message": f"Esta é a rota de visualizacao do detalhe do id do produto {product_id}"
+            "message": f"Esta é a rota de visualizacao do detalhe do id do produto {id_produto}"
         }
     )
 
 
 # RF: O sistema deve permitir a atualizacao de um unico produto e produto existente
-@main_bp.route("/product/<int:product_id>", methods=["PUT"])
-def update_product(product_id):
+@main_bp.route("/produto/<int:id_produto>", methods=["PUT"])
+def update_produto(id_produto):
     return jsonify(
-        {"message": f"Esta é a rota de atualizacao do produto com o id {product_id}"}
+        {"message": f"Esta é a rota de atualizacao do produto com o id {id_produto}"}
     )
 
 
 # RF: O sistema deve permitir a delecao de um unico produto e produto existente
-@main_bp.route("/product/<int:product_id>", methods=["DELETE"])
-def delete_product(product_id):
+@main_bp.route("/produto/<int:id_produto>", methods=["DELETE"])
+def delete_produto(id_produto):
     return jsonify(
-        {"message": f"Esta é a rota de deleção do produto com o id {product_id}"}
+        {"message": f"Esta é a rota de deleção do produto com o id {id_produto}"}
     )
 
 
@@ -59,12 +69,17 @@ def login():
     except Exception as e:
         return jsonify({"error": "Erro durante a requisição do dado"}), 500
 
+    if user_data.username == "admin" and user_data.password == "123":
+        return jsonify({"message": "Login bem-sucedido!"})
+    else:
+        return jsonify({"message": "Credenciais invalidas!"})
+
     return jsonify(
         {"message": f"Realizar o login do usuario {user_data.model_dump_json()}"}
     )
 
 
 # RF: O sistema deve permitir a importacao de vendas através de um arquivo
-@main_bp.route("/sales/upload", methods=["POST"])
-def upload_sales():
+@main_bp.route("/vendas/upload", methods=["POST"])
+def upload_vendas():
     return jsonify({"message": "Esta é a rota de upload do arquivo de vendas"})
